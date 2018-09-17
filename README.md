ee50fa20a8ef525e475969288807b438e493be0c
mk@Ehrmantraut <37540514+mkfallen@users.noreply.github.com>
2018-09-17 10:40:44 +0300

    VK authentication

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Install [Heroku Toolbelt](https://toolbelt.heroku.com/), then:
 1. Edit [bot.py](https://github.com/Kylmakalle/heroku-telegram-bot/blob/master/bot.py) file with your code
 
     1. **ATTENTION!** Do not collapse/delete/comment `some_token = os.environ[SOME_TOKEN]` style stings _(you can delete redis setup line if you do not need it)_, **do not change them with your REAL tokens**, all tokens will be setted up below in this guide!
-    
+
     2. [More About Config Vars](https://devcenter.heroku.com/articles/config-vars)
     3. Also, don't do like [this](http://i.imgur.com/Yni1jZX.png), it's insecure, **realy.**
 
@@ -34,7 +34,7 @@ heroku ps:stop bot #stop bot dyno
 ```
 
 ## Deploying via [Heroku Dashboard](https://dashboard.heroku.com) (GUI)
-1. [Fork](https://github.com/Kylmakalle/heroku-telegram-bot/fork) this repo to your account. 
+1. [Fork](https://github.com/Kylmakalle/heroku-telegram-bot/fork) this repo to your account.
 2. [Edit files](https://github.com/Kylmakalle/heroku-telegram-bot#edit-files)
 3. Go to [Dashboard](https://dashboard.heroku.com), login, Press _New_ and choose _Create new app._
 4. Fill in an _App Name_ and choose _Runtime Region._
@@ -47,8 +47,8 @@ heroku ps:stop bot #stop bot dyno
     1. Install _Heroku Redis_ add-on _(Optionaly)_
     2. Press on a small pen button, move slider and then click _Confirm_, that will start bot dyno.
     3. Simply move slider back if you need to stop bot dyno, remember to click _Confirm_.
-    4. If for some reason it’s not working, check the logs here 
-    
+    4. If for some reason it’s not working, check the logs here
+
     ![Logs](http://i.imgur.com/rIHU6zF.png)
 
 ### More about
--- bot.py
+++ bot.py
@@ -1,20 +1,98 @@
 # -*- coding: utf-8 -*-
-import redis
+import logging
 import os
-import telebot
-# import some_api_lib
-# import ...
+import re
+import requests
+import vk_api as vk
+import telebot as tg
 
-# Example of your code beginning
-#           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
+tg_token = os.environ['TELEGRAM_TOKEN']
+proxy = os.environ['PROXY_LINK']
+app_id = os.environ['VK_CLIENT_ID']
+sessions = dict()
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
+if proxy:
+    tg.apihelper.proxy = {'https': proxy}
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+bot = tg.TeleBot(tg_token)
+tg.logger.setLevel(logging.DEBUG)
+
+
+def get_user_input(chat_id, text):
+    bot.send_message(chat_id, text)
+
+    updates = bot.get_updates(timeout=20)
+    return updates
+
+
+@bot.message_handler(commands=['auth'])
+def auth(m):
+    if m.chat.id in sessions:
+        vk_api = get_vk_api(m.chat.id, sessions[m.chat.id])
+        user = vk_api.account.getProfileInfo()
+        str = 'Already authorized: '+user['first_name']+' '+user['last_name']
+        bot.send_message(m.chat.id, str)
+        return
+
+    params = {
+        'client_id': app_id,
+        'redirect_uri': 'https://oauth.vk.com/blank.html',
+        'scope': 4096 + 2,
+        'v': '5.84',
+        'display': 'page',
+        'response_type': 'token',
+        'revoke': 1,
+        }
+    res = requests.get('https://oauth.vk.com/authorize', params=params)
+    str = "Authorize here and send back the access token: \n"+res.url
+
+    r = bot.send_message(m.chat.id, str)
+    bot.register_next_step_handler(r, get_vk_token)
+
+
+def get_vk_token(m):
+    vk_token = None
+    text = m.text
+    token_match = re.match(r"^\w{85}$", text)
+    link_match = re.search(r"^https?://.*access_token=(\w{85}).*$", text)
+    if token_match:
+        bot.send_message(m.chat.id, "Received a token: "+text)
+        vk_token = text
+    elif link_match:
+        bot.send_message(m.chat.id, "Received a token: "+link_match.group(1))
+        vk_token = link_match.group(1)
+    else:
+        bot.send_message(m.chat.id, "Wrong token.")
+
+    if vk_token:
+        vk_api = get_vk_api(m.chat.id, vk_token)
+        user = vk_api.account.getProfileInfo()
+        str = 'Authorized: '+user['first_name']+' '+user['last_name']
+        bot.send_message(m.chat.id, str)
+
+
+def get_vk_api(cid, vk_token):
+    session = vk.VkApi(token=vk_token, app_id=app_id)
+    vk_api = session.get_api()
+    try:
+        sessions[cid] = vk_token
+        return vk_api
+    except vk.ApiError as e:
+        bot.send_message(cid, e)
+        print(e)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(m):
+    bot.reply_to(m, "I'm a bot!")
+
+
+@bot.message_handler(commands=['hello'])
+def hello(m):
+    bot.send_message(m.chat.id, f"Hello, {m.from_user.first_name}")
+
+
+# bot.enable_save_next_step_handlers(delay=0)
+# bot.load_next_step_handlers()
+
+bot.polling()
